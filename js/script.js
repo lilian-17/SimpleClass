7e364b4a6827cfb51680bd467f0ff07e2306952f
mtsii <130447596+mtsii@users.noreply.github.com>
2025-01-13 11:53:28 +0100

    Style bouton thème changé + Enregistrement thème choisi a travers les pages + Changement ecriture sur le bouton

--- js/script.js
+++ js/script.js
@@ -1,6 +1,34 @@
 function togglemode() {
 
-    console.log('test')
-    var body = document.body;
-    body.classList.toggle("dark-mode");
+  console.log('Thème changé')
+  var body = document.body;
+  body.classList.toggle("dark-mode");
+
+  var button = document.getElementById("toggleButton");
+
+  console.log(button.value)
+  if (button.value === "Thème clair") {
+    button.value = "Thème sombre";
+    localStorage.setItem("theme", "dark-mode");
   }
+
+  else {
+    button.value = "Thème clair";
+    localStorage.setItem("theme", "");
+  }
+
+}
+
+window.onload = function() {
+  var savedTheme = localStorage.getItem("theme");
+  var body = document.body;
+  var button = document.getElementById("toggleButton");
+
+  if (savedTheme === "dark-mode") {
+      body.classList.add("dark-mode");
+      button.value = "Thème sombre";
+  } else {
+      body.classList.remove("dark-mode");
+      button.value = "Thème clair";
+  }
+};
