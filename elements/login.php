082ed4ce6026198b41ec7fe8a79f66d9ca2fbd51
mtsii <130447596+mtsii@users.noreply.github.com>
2025-04-24 16:57:17 +0200

    ajout login.php register.php connection.php

---
+++ elements/login.php
@@ -0,0 +1,19 @@
+<?php
+    echo"
+        <h1>Se connecter</h1>
+
+        <form>
+            <label for='loginUsername'>Nom : </label>
+            <input id='loginUsername' type='text'>
+            
+            <label for='loginEmail'>Email : </label>
+            <input id='loginEmail' type='mail'>
+
+            <label for='loginPW'>Mot de passe : </label>
+            <input id='loginPW' type='text'>
+
+            <input id='loginSubmit' type='submit'>
+
+        </form>
+"
+?>
---
+++ elements/register.php
@@ -0,0 +1,10 @@
+<?php
+    echo"
+        <h1>S'inscrire</h1>
+
+        <form>
+            <input type='text'>
+            <input type='text'>
+        </form>
+"
+?>
